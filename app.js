a3cf46188b645f8b87b0d9f2901a73feff54fa1e
KhineLinTun <khinglintun@KhinelintunMacBook-Air.local>
2025-02-01 15:12:36 +0700

    Setup Vue CND and create html file and js file and testing creat app and dynamic data

---
+++ app.js
@@ -0,0 +1,10 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            title:"You Don't Know Js",
+            author:"Kyle Simpon",
+            age: 54
+        }
+    }
+});
+app.mount('#app ')

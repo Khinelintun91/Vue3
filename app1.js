2c70de494f47f829c420f2f1ba04224f1b93eade
KhineLinTun <khinglintun@KhinelintunMacBook-Air.local>
2025-02-02 10:48:02 +0700

    In Lesson2 learn how to hide and show dynamicaly

--- app1.js
+++ app1.js
@@ -3,7 +3,8 @@ const app=Vue.createApp({
         return{
             title:"You Don't Know Js",
             author:"Kyle Simpon",
-            age: 54
+            age: 54,
+            showBooks: true
         }    
     },
     methods:{
